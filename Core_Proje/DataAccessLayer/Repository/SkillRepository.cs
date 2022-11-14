a57b6d471a3bc3efbd84b59998907a654485d762
Ömer Faruk Duran <omerfarukdrn@gmail.com>
2022-11-14 20:10:34 +0300

    Blog sitesi

---
+++ Core_Proje/DataAccessLayer/Repository/SkillRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class SkillRepository : IGenericDal<Skill>
+    {
+        public void Delete(Skill t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Skill GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Skill> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Skill t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Skill t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
